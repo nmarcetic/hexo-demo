b656e4f0c28a484bcaf031ba325e06f379ae23cd
Nicolas Lair <nicolas.lair@gmail.com>
2016-07-07 16:57:52 +0200

    Application Page Generation

---
+++ themes/mfac/scripts/application-pages-generator.js
@@ -0,0 +1,10 @@
+hexo.extend.generator.register('application-pages', function(locals) {
+	return locals.mfac_api_data.applicationList.map(function(app) {
+		console.log("app.name>",app.name);
+		return {
+			path : 'applications/' + app.name.toLowerCase().replace(/\s+/g, '-') + '.html',
+			data : app,
+			layout : 'application'
+		};
+	});
+});
